a4f558d582e16bb8b28ce2b538fde011af15b983
Isabella Xiong <IXiong@ucvts.tec.nj.us>
2019-10-29 10:48:54 -0400

    problem 1

--- src/change.js
+++ src/change.js
@@ -2,13 +2,18 @@ const readlineSync = require("readline-sync");
 
 const MIN = 0.01;
 const MAX = 0.99;
+let quarter= 0;
+let dime= 0;
+let nickel= 0;
+let penny= 0;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid.");
 } else {
-    // write your code for making change here
+
+console.log( quarter + " quarters, " + dime + " dimes, " + nickel + " nickels, " + penny + " pennies.");
 }
